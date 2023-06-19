297c5304bdc31a298fd776c4999adb985d674719
oskuda <oscarstha@live.com>
2023-06-19 09:52:00 +0530

    initial commit

---
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/BookStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package np.com.oskarshrestha.bookstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookStoreBackendApplication.class, args);
+	}
+
+}
