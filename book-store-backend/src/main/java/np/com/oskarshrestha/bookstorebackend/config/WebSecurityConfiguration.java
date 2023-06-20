103ffde772eea47db61f9fc1e261cd2457cb724f
oskuda <oscarstha@live.com>
2023-06-20 10:48:51 +0530

    added jwt token service and configs;

--- book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/config/WebSecurityConfiguration.java
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/config/WebSecurityConfiguration.java
@@ -1,11 +1,14 @@
 package np.com.oskarshrestha.bookstorebackend.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -16,6 +19,12 @@ public class WebSecurityConfiguration {
             "/api/v1/books",
             "/api/v1/register"
     };
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Autowired
+    private AuthenticationProvider authenticationProvider;
 
 
     @Bean
@@ -32,10 +41,10 @@ public class WebSecurityConfiguration {
                 .authenticated()
                 .and()
                 .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//                .and()
-//                .authenticationProvider(authenticationProvider)
-//                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 }
--- book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/AuthenticationController.java
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/AuthenticationController.java
@@ -7,15 +7,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class AuthenticationController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/api/v1/register")
+    @PostMapping("/register")
     public ResponseEntity<UserRegisterResponse> registerUser(
             @RequestBody UserRegisterRequest userRegisterRequest
     ){
