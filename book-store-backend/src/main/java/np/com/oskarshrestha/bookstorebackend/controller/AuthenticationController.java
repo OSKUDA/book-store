aaace48cc44c5afd0bc251a9a7ead303ce86c159
oskuda <oscarstha@live.com>
2023-06-22 22:41:08 +0530

    added support for CORS for react app

--- book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/AuthenticationController.java
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/AuthenticationController.java
@@ -7,12 +7,10 @@ import np.com.oskarshrestha.bookstorebackend.model.UserRegisterResponse;
 import np.com.oskarshrestha.bookstorebackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/api/v1")
 public class AuthenticationController {
 
--- book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/TestController.java
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/TestController.java
@@ -1,9 +1,11 @@
 package np.com.oskarshrestha.bookstorebackend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 public class TestController {
 
     @GetMapping("/api/v1/hello")
