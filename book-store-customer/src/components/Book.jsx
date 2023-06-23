ec191923145e887b104fb5e024735ee7fcb60ef8
oskuda <oscarstha@live.com>
2023-06-23 11:59:20 +0530

    minor ui changes on book card;

--- book-store-customer/src/components/Book.jsx
+++ book-store-customer/src/components/Book.jsx
@@ -1,9 +1,9 @@
 const Book = ({ minBook }) => {
   return (
     <div className="book-card">
-      <h3 className="book-title">{minBook.title}</h3>
-      <p className="book-author">{minBook.author}</p>
-      <p className="book-publication">{minBook.publicationDate}</p>
+      <h3 className="book-title">&quot;{minBook.title}&quot;</h3>
+      <p className="book-author">— {minBook.author}</p>
+      <p className="book-publication">year: {minBook.publicationDate}</p>
       <p className="book-price">₹{minBook.price}</p>
       <p className="book-amount">Available: {minBook.quantity}</p>
     </div>
