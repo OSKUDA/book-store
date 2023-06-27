66d5c1a0b80695ca203bc924e63bc48efbfd14b1
oskuda <oscarstha@live.com>
2023-06-28 01:05:25 +0530

    added get api to fetch all min user

---
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/model/MinUsersResponse.java
@@ -0,0 +1,18 @@
+package np.com.oskarshrestha.bookstorebackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MinUsersResponse {
+    private boolean status;
+    private String message;
+    private List<MinUser> minUserList;
+}
