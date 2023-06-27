01d3fff777b07f194192ecda0baa92d5d4d01d1b
oskuda <oscarstha@live.com>
2023-06-27 14:27:12 +0530

    added order by email impl

--- book-store-customer/src/components/Auth/SignIn.jsx
+++ book-store-customer/src/components/Auth/SignIn.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserDetailContext } from "../../contexts/UserDetails";
 import validateEmailPassword from "../../utils/validateEmailPassword";
