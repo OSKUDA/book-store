eb6bc8fc1b30d251e2ff7ed6f99f610e7d1f1736
oskuda <oscarstha@live.com>
2023-08-06 09:30:55 +0530

    updated api url

--- book-store-admin/src/services/users/getUsers.js
+++ book-store-admin/src/services/users/getUsers.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const getUsers = ({ query }) => {
   const token = query[0];
-  const url = `http://localhost:8080/api/v1/users`;
+  const url = `http://localhost:8080/api/v1/admin/users`;
   return axios.get(url, {
     headers: { Authorization: `Bearer ${token.replace(/^"(.*)"$/, "$1")}` },
   });
