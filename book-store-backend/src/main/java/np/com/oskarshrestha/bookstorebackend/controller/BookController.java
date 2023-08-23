85841ca8a3595ae366f473d72ce497bddf9e6234
oskuda <oscarstha@live.com>
2023-08-23 16:36:32 +0530

    refactored BookService with generic response;

--- book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/BookController.java
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/BookController.java
@@ -1,16 +1,13 @@
 package np.com.oskarshrestha.bookstorebackend.controller;
 
 import np.com.oskarshrestha.bookstorebackend.request.AddBookRequest;
-import np.com.oskarshrestha.bookstorebackend.response.AddBookResponse;
-import np.com.oskarshrestha.bookstorebackend.response.BookResponse;
-import np.com.oskarshrestha.bookstorebackend.response.BooksResponse;
 import np.com.oskarshrestha.bookstorebackend.service.BookService;
+import np.com.oskarshrestha.bookstorebackend.util.ResponseModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:5173/")
 @RequestMapping("/api/v1")
 public class BookController {
 
@@ -18,42 +15,29 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping("/book")
-    public ResponseEntity<BookResponse> getBookById(
+    public ResponseEntity<ResponseModel> getBookById(
             @RequestParam("id") long id
     ) {
-        BookResponse response = bookService.fetchBookById(id);
-        if (response.getBook() != null) {
-            return ResponseEntity.ok(response);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        ResponseModel response = bookService.fetchBookById(id);
+        return new ResponseEntity<>(response, response.getHttpStatus());
     }
 
     @PostMapping("/book")
-    public ResponseEntity<AddBookResponse> postBook(
+    public ResponseEntity<ResponseModel> postBook(
             @RequestBody AddBookRequest addBookRequest
     ) {
-        AddBookResponse response = bookService.addBook(addBookRequest);
-        if (response.isStatus()) {
-            return ResponseEntity.ok(response);
-        } else {
-            return ResponseEntity.status(409).body(response);
-        }
+        ResponseModel response = bookService.addBook(addBookRequest);
+        return new ResponseEntity<>(response, response.getHttpStatus());
     }
 
 
-
     @GetMapping("/books")
-    public ResponseEntity<BooksResponse> getBooks(
+    public ResponseEntity<ResponseModel> getBooks(
             @RequestParam("page") int page,
             @RequestParam("length") int length
     ) {
-        BooksResponse response = bookService.fetchBooks(page, length);
-        if (response.getBookList() != null) {
-            return ResponseEntity.ok(response);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        ResponseModel response = bookService.fetchBooks(page, length);
+        return new ResponseEntity<>(response, response.getHttpStatus());
     }
 
 
--- book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/service/BookService.java
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/service/BookService.java
@@ -1,15 +1,13 @@
 package np.com.oskarshrestha.bookstorebackend.service;
 
 import np.com.oskarshrestha.bookstorebackend.request.AddBookRequest;
-import np.com.oskarshrestha.bookstorebackend.response.AddBookResponse;
-import np.com.oskarshrestha.bookstorebackend.response.BookResponse;
-import np.com.oskarshrestha.bookstorebackend.response.BooksResponse;
+import np.com.oskarshrestha.bookstorebackend.util.ResponseModel;
 
 public interface BookService {
-    BookResponse fetchBookById(Long id);
+    ResponseModel fetchBookById(Long id);
 
-    BooksResponse fetchBooks(int page, int length);
+    ResponseModel fetchBooks(int page, int length);
 
-    AddBookResponse addBook(AddBookRequest addBookRequest);
+    ResponseModel addBook(AddBookRequest addBookRequest);
 
 }
