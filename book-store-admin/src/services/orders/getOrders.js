b2b3b0672a840556de24be34a06fc282773c867e
oskuda <oscarstha@live.com>
2023-08-27 13:33:56 +0530

    updated api url

--- book-store-admin/src/services/orders/getOrders.js
+++ book-store-admin/src/services/orders/getOrders.js
@@ -3,7 +3,7 @@ const getOrders = ({ query }) => {
   const token = query[0];
   const page = query[1];
   const length = query[2];
-  const url = `http://localhost:8080/api/v1/orders?page=${page}&length=${length}`;
+  const url = `http://localhost:8080/api/v1/admin/orders?page=${page}&length=${length}`;
   return axios.get(url, {
     headers: { Authorization: `Bearer ${token.replace(/^"(.*)"$/, "$1")}` },
   });
