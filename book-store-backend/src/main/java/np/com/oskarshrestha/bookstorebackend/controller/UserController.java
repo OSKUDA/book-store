8a2e96d8761a918bc4da7d7a3306a198429324d3
oskuda <oscarstha@live.com>
2023-08-27 13:53:12 +0530

    removed email parameter from GET /user api

--- book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/UserController.java
+++ book-store-backend/src/main/java/np/com/oskarshrestha/bookstorebackend/controller/UserController.java
@@ -1,24 +1,23 @@
 package np.com.oskarshrestha.bookstorebackend.controller;
 
-import np.com.oskarshrestha.bookstorebackend.response.MinUserResponse;
 import np.com.oskarshrestha.bookstorebackend.service.UserService;
 import np.com.oskarshrestha.bookstorebackend.util.ResponseModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
     @Autowired
     private UserService userService;
+
     @GetMapping("/user")
     public ResponseEntity<ResponseModel> getMinUserDetails(
-            @RequestParam("email") String email
-    ){
-        ResponseModel response = userService.fetchMinUser(email);
+    ) {
+        ResponseModel response = userService.fetchMinUser();
         return new ResponseEntity<>(response, response.getHttpStatus());
     }
-
-
 }
