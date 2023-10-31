aff2794877bd9d3a368c1974f8ab7ff3142d0219
Oskar Krishna Shrestha <41532513+OSKUDA@users.noreply.github.com>
2023-10-31 09:20:29 +0530

    External package for app aspect

---
+++ aop-logging/src/main/java/np/com/oskarshrestha/bookstorebackend/aspect/AppAspect.java
@@ -0,0 +1,6 @@
+package np.com.oskarshrestha.bookstorebackend.aspect;/**
+ * @author Oskar Krishna Shrestha
+ * date: 10/20/2023
+ * package: np.com.oskarshrestha.bookstorebackend.aspect
+ */public class AppAspect {
+}
---
+++ aop/src/main/java/AppAspect.java
@@ -0,0 +1,6 @@
+package PACKAGE_NAME;/**
+ * @author Oskar Krishna Shrestha
+ * date: 10/17/2023
+ * package: PACKAGE_NAME
+ */public class AppAspect {
+}
